bcc93cee56745aba78c83254d919bef8efcbaf68
idbeny <>
2023-02-20 11:54:11 +0800

    feaet:update

---
+++ CICamera/CICamera.framework/Headers/CICameraMarco.h
@@ -0,0 +1,18 @@
+//
+//  CICameraMarco.h
+//  CICamera
+//
+//  Created by 大大大大_荧🐾 on 2020/2/23.
+//  Copyright © 2020 大大大大_荧🐾. All rights reserved.
+//
+
+#ifndef CICameraMarco_h
+#define CICameraMarco_h
+
+typedef enum CameraMode {
+    MODE_PICTURE = 1,
+    MODE_VIDEO,
+    MODE_SCAN
+} CameraMode;
+
+#endif /* CICameraMarco_h */
